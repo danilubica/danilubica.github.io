ee242f8d351f1a33b3dded843d14419af9bbdd5c
daniil <dvromin@yandex.ru>
2023-06-06 21:15:37 +0700

    secondScr

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
+
+
+    let element = document.getElementById('phone');
+    let maskOptions = {
+    mask: '+{7}(000)000-00-00'
+};
+    let mask = IMask(element, maskOptions);
+
+
 function myFunction() {
     document.getElementById("myDropdown").classList.toggle("show");
 }
